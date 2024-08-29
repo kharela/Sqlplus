8d22e2bc576954934bf9b2bb8babdd1c8816707a
kharela <kharela@hotmail.com>
2024-08-29 15:57:28 -0400

    Update qsql_active.sql
    v2

--- qsql_active.sql
+++ qsql_active.sql
@@ -1,18 +1,26 @@
 -- qsql_active
 
 -- parameters:
--- pass in a regexp pattern for username
+-- 1 -  regexp pattern for username
 
-set lines 999
+-- usage:
+-- qsql_active "user1|kh[a,u]..la|^c##"
 
-col username for a12
+
+--set defaults for parametersms
 col fltr_username new_value v_username
-
-col sql_text for a100 
-
 select case when '&1' is null then '.*' else '&1' end as fltr_username from dual;
 
+--custom settings
+set lines 999
+col username for a12
+col sql_text for a100 
+
+--query
 select username, sid, serial#, p.sql_id, s.sql_text
    from v$session p, v$sql s
    where regexp_like(username, '&v_username', 'i')
      and p.sql_id = s.sql_id;
+
+
+--exit settings
