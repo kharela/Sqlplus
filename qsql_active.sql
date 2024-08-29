bf456e9b56a2b70bbd6406a8ba991b04de3d694b
kharela <kharela@hotmail.com>
2024-08-29 14:37:53 -0400

    Create qsql_active.sql
    v1

---
+++ qsql_active.sql
@@ -0,0 +1,18 @@
+-- qsql_active
+
+-- parameters:
+-- pass in a regexp pattern for username
+
+set lines 999
+
+col username for a12
+col fltr_username new_value v_username
+
+col sql_text for a100 
+
+select case when '&1' is null then '.*' else '&1' end as fltr_username from dual;
+
+select username, sid, serial#, p.sql_id, s.sql_text
+   from v$session p, v$sql s
+   where regexp_like(username, '&v_username', 'i')
+     and p.sql_id = s.sql_id;
